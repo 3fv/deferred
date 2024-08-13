806475cf1140d2fe4453ac085241078925326b13
Jonathan Glanz <jonathan.glanz@gmail.com>
2024-08-12 21:03:47 -0400

    wip

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
     verbose: true,
-    testRegex: "src\\/.*\\.spec\\.(ts|tsx)$",
+    testRegex: "src/test/.*\\.spec\\.(ts|tsx)$",
     moduleDirectories: [
       "node_modules"
     ],
